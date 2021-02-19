111921b1805eb7da5cdc244003b3369d51534be5
Indrajeet Patil <inderonline1988@gmail.com>
2021-02-19 14:23:06 +0100

    line up vertically

--- docs/slides/images/outlier_example.R
+++ docs/slides/images/outlier_example.R
@@ -9,8 +9,15 @@ df <-
     weight = c(c(rnorm(20, mean = 24)), c(rnorm(14, mean = 24), 80, 87, 85, 45, 55, 150))
   )
 
-ggplot2::ggsave(ggbetweenstats(df, group, weight, type = "p"),
-  filename = paste0(here::here(), "/docs/slides/images/after_ggstats.PNG")
+ggplot2::ggsave(ggbetweenstats(df, group, weight, type = "p",
+                               ggtheme = hrbrthemes::theme_ipsum_tw(),
+                               title = "Effect of intervention on weight"),
+  filename = paste0(here::here(), "/docs/slides/images/after_ggstats.PNG"),
+  height = 5,
+  width = 6,
+  units = "in"
 )
 
-ggbetweenstats(df, group, weight, type = "r")
+ggbetweenstats(df, group, weight, type = "r",
+               ggtheme = hrbrthemes::theme_ipsum_tw(),
+               title = "Effect of intervention on weight")
